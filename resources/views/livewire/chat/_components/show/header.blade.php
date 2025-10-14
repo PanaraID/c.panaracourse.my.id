452f0f6e67c74c76802c6b7497a8db65ee277509
ahlaiptek <iptekahla@gmail.com>
2025-10-14 08:41:43 +0700

    Remove unnecessary transition duration from new message notification button

--- resources/views/livewire/chat/_components/show/header.blade.php
+++ resources/views/livewire/chat/_components/show/header.blade.php
@@ -134,7 +134,7 @@
                 }
             </script>
              <div id="new-message-received" onclick="scrollToBottomBre()"
-                 class="hidden bg-gradient-to-r cursor-pointer from-yellow-400 via-yellow-500 to-yellow-600 dark:from-yellow-600 dark:via-yellow-700 dark:to-yellow-800 text-black dark:text-white px-4 py-2 rounded-xl shadow-lg flex items-center gap-2 transition-all duration-300">
+                 class="hidden bg-gradient-to-r cursor-pointer from-yellow-400 via-yellow-500 to-yellow-600 dark:from-yellow-600 dark:via-yellow-700 dark:to-yellow-800 text-black dark:text-white px-4 py-2 rounded-xl shadow-lg flex items-center gap-2">
                  <svg class="w-5 h-5 text-black dark:text-white animate-bounce" fill="none" stroke="currentColor"
                      viewBox="0 0 24 24">
                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
