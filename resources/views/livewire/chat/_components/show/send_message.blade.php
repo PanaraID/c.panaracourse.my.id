4ca3385e231f3915f81f38af2f4813339179b496
ahlaiptek <iptekahla@gmail.com>
2025-10-20 11:24:58 +0700

    Remove maximum file size display from send message component

--- resources/views/livewire/chat/_components/show/send_message.blade.php
+++ resources/views/livewire/chat/_components/show/send_message.blade.php
@@ -398,7 +398,8 @@ new class extends Component {
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" 
                     d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
-            <span>Ukuran file maksimal: <strong class="text-emerald-600 dark:text-emerald-400">1 GB</strong></span>
+            {{-- <span>Ukuran file maksimal: <strong class="text-emerald-600 dark:text-emerald-400">1 GB</strong></span> --}}
+            {{-- FIXME --}}
         </div>
 
         <!-- 📝 Input Pesan - PLAIN TEXT TEXTAREA -->
