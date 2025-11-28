8b9e7485d05bce7f93379a3c5f764f2e60a33db1
ahlaiptek <iptekahla@gmail.com>
2025-11-28 07:43:04 +0700

    feat: Add Laravel Horizon integration with service provider and configuration

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,6 +3,7 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\FortifyServiceProvider::class,
+    App\Providers\HorizonServiceProvider::class,
     App\Providers\VoltServiceProvider::class,
     Barryvdh\Debugbar\ServiceProvider::class,
 ];
